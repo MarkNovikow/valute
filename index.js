7fdd580b371f68b221becd5ce7bf1391cb1f945d
Mark Novikov <marknoviko@mail.ru>
2023-06-19 13:04:02 +0300

    currency selection implemented

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ numInput.value = 1;
 const resInput = document.querySelector('.result-input');
 resInput.disabled = true;
 let valuteCourseData = {};
-const button = document.querySelector('.select-list');
+const button = document.querySelector('.select-list-button');
 let promise = fetch('https://www.cbr-xml-daily.ru/daily_json.js').
 then(res => res.json()).
 then(res => {
@@ -22,5 +22,31 @@ then(res => {
     numInput.dispatchEvent(new Event('input'));
 })
 
-
 numInput.addEventListener('input', makeCount);
+
+let selectList = document.createElement('div');
+selectList.classList.add('select-list');
+promise.then(() => {
+    for (const el in valuteCourseData) {
+        selectList.innerHTML += '<div class="select-list-item">' + valuteCourseData[el].Name + '</div>';
+    }
+    const valutes = document.querySelectorAll('.select-list-item');
+    for (const valuteKey of valutes) {
+            valuteKey.addEventListener('click', (ev) => {
+                button.innerText = ev.currentTarget.innerText;
+                numInput.dispatchEvent(new Event('input'));
+        })
+    }
+})
+
+button.insertAdjacentElement("afterend", selectList);
+
+
+button.addEventListener('click', (ev) => {
+    selectList.classList.toggle('select-list-visible');
+});
+document.addEventListener('click', (ev) => {
+    if (ev.target.className != 'select-list-item' && ev.target != button) {
+        selectList.classList.remove('select-list-visible')
+    }
+})
