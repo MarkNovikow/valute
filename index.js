fb1e08bbf401744ce90c3a222f22befb8964b20a
mark novikov <marknoviko@mail.ru>
2023-06-21 13:47:42 +0300

    js code refactored

--- index.js
+++ index.js
@@ -1,48 +1,22 @@
-const makeCount = ((ev) => {
-if (isNaN(ev.target.value)) {
-    resInput.value = "Неверные входные данные";
-    return;
-}
+const button = document.querySelector('.select-list-button');
+const numInput = document.querySelector('.num-input');
+const resInput = document.querySelector('.result-input');
+let valuteCourseData = {};
+let selectList = document.createElement('div');
+selectList.classList.add('select-list');
+numInput.value = 1;
+resInput.disabled = true;
+const handleMakeCount = ((ev) => {
+    if (isNaN(ev.target.value)) {
+        resInput.value = "Неверные входные данные";
+        return;
+    }
     for (const key in valuteCourseData) {
-        if(valuteCourseData[key].Name === button.innerText) {
+        if (valuteCourseData[key].Name === button.innerText) {
             resInput.value = numInput.value * valuteCourseData[key].Value
         }
     }
 })
-const numInput = document.querySelector('.num-input');
-numInput.value = 1;
-const resInput = document.querySelector('.result-input');
-resInput.disabled = true;
-let valuteCourseData = {};
-const button = document.querySelector('.select-list-button');
-let promise = fetch('https://www.cbr-xml-daily.ru/daily_json.js').
-then(res => res.json()).
-then(res => {
-    valuteCourseData = res.Valute;
-    numInput.dispatchEvent(new Event('input'));
-})
-
-numInput.addEventListener('input', makeCount);
-
-let selectList = document.createElement('div');
-selectList.classList.add('select-list');
-promise.then(() => {
-    for (const el in valuteCourseData) {
-        selectList.innerHTML += '<div class="select-list-item">' + valuteCourseData[el].Name + '</div>';
-    }
-    const valutes = document.querySelectorAll('.select-list-item');
-    for (const valuteKey of valutes) {
-            valuteKey.addEventListener('click', (ev) => {
-                button.innerText = ev.currentTarget.innerText;
-                numInput.dispatchEvent(new Event('input'));
-                selectList.classList.toggle('select-list-visible');
-        })
-    }
-})
-
-button.insertAdjacentElement("afterend", selectList);
-
-
 button.addEventListener('click', (ev) => {
     selectList.classList.toggle('select-list-visible');
 });
@@ -51,3 +25,25 @@ document.addEventListener('click', (ev) => {
         selectList.classList.remove('select-list-visible')
     }
 })
+numInput.addEventListener('input', handleMakeCount);
+let promise = fetch('https://www.cbr-xml-daily.ru/daily_json.js').then(res => res.json()).then(res => {
+    valuteCourseData = res.Valute;
+    numInput.dispatchEvent(new Event('input'));
+})
+promise.then(() => {
+    for (const el in valuteCourseData) {
+        selectList.innerHTML += '<div class="select-list-item">' + valuteCourseData[el].Name + '</div>';
+    }
+    const valutes = document.querySelectorAll('.select-list-item');
+    for (const valuteKey of valutes) {
+        valuteKey.addEventListener('click', (ev) => {
+            button.innerText = ev.currentTarget.innerText;
+            numInput.dispatchEvent(new Event('input'));
+            selectList.classList.toggle('select-list-visible');
+        })
+    }
+})
+
+button.insertAdjacentElement("afterend", selectList);
+
+
